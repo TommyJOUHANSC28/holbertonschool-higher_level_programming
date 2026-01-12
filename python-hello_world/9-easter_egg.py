da167afafb8408d2060ebf031e62f876317ebfd5
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-12 15:52:10 +0000

    Afficher The Zen of python

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,1 +1,4 @@
-import this;print("The Zen of Python, by Tim Peters\n"+''.join([this.d.get(c,c)for c in this.s]))
+#!/usr/bin/python3
+import this
+zen_de_python = "".join([this.d.get(c, c) for c in this.s])
+print(zen_de_python)
