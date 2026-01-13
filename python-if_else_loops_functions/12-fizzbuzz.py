5601c9bf42087c23633ff1a036cf067d2cddc9bd
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-13 19:27:54 +0100

    Fizz Buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        fizz =  i % 3
+        buzz =  i % 5
+        if fizz == 0 and buzz == 0:
+            print("FizzBuzz", end='')
+        elif fizz == 0:
+            print("Fizz", end='')
+        elif buzz == 0:
+            print("Buzz", end='')
+        else:
+            print(i, end ='')
+
+        print(" ", end='')
