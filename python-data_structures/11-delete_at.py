1e0eeb4626bf8de42d70ea43a9cef01cb2750053
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-19 13:38:21 +0000

    delete at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None:
-        return None
+    lenghtList = len(my_list)
 
     if idx < 0 or idx >= lenghtList:
         return (my_list)
 
-    new_list = my_list.copy()
     del my_list[idx]
 
     return (my_list)
