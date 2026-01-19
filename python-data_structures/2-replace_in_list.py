0678750ec15d34322d6371cbb99ee7c706349db4
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-19 09:25:06 +0000

    fonction qui extrait un élément d'une liste.

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return (None)
+        return (my_list)
 
     lenghtList = len(my_list)
 
     if idx > lenghtList - 1:
-        return (None)
+        return (my_list)
 
     my_list[idx] = element
 
