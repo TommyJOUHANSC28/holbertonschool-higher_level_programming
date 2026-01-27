75c9aef88222a4774c670b463a153ba1cff0cd89
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-27 10:15:52 +0000

    Area and Perimeter

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Define a rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle that defines a square."""
+
+    def __init__(self, width=0, height=0):
+        """Mehod to initialize the square object
+        """
+        self.width = width
+        self.height = height
+
+        if height == 0 or width == 0:
+            return 0
+
+    @property
+    def width(self):
+        """Method to return the value of the rectangle
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Method that sets the position value of a rectangle objet
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Method to return the value of the rectangle
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Method that sets the position value of a rectangle objet
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Method  that returns the rectangle area
+        """
+        return ((self.__height) * (self.__width))
+
+    def perimeter(self):
+        """that returns the rectangle perimeter
+        """
+        return (2 * ((self.__height) + (self.__width)))
