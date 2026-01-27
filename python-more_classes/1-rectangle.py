d61d219c2285d8eb476d799de1573696d7c2f92e
Tommy JOUHANS <12119@holbertonstudents.com>
2026-01-27 09:29:00 +0000

    Real definition of a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Define a rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle that defines a square."""
+
+    def __init__(self, width=0, height=0):
+        """Mehod to initialize the square object
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Method to return the value of the rectangle
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Method that sets the position value of a rectangle objet
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Method to return the value of the rectangle
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Method that sets the position value of a rectangle objet
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
